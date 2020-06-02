0515f032c8eab1852cf520726d6760628ab1138f
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-02 16:40:22 -0700

    updates on post routes

---
+++ routes/api/post-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+
+
+//get all users
+router.get('/', (req, res) => {
+    console.log("=========================");
+    Post.findAll({
+        //Query configuration
+
+    })
+});
+
+
+module.exports = router;
