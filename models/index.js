e6dd125300a4ba5e95b21ce9ea1a05f04518fc09
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-03 16:15:06 -0700

    changes to vote models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 //create associations
 User.hasMany(Post, {
@@ -12,5 +13,35 @@ Post.belongsTo(User, {
     // onDelete: "cascade"
 });
 
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
 
-module.exports = { User, Post };
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+})
+
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+
+
+module.exports = { User, Post, Vote };
