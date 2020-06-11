a8ec011f9dde4d0543913b90b6f667cf481508e0
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-10 23:19:58 -0700

    latest changes after 14.5.3

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,96 +1,3 @@
-// const router = require('express').Router();
-
-// router.get('/login', (req, res) => {
-//     if (req.session.loggedIn) {
-//       res.redirect('/');
-//       return;
-//     }
-  
-//     res.render('login');
-//   });
-
-
-// router.get('/', (req, res) => {
-//     console.log(req.session);
-
-//     res.render('homepage', {
-//         id: 1, 
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebar Docs',
-//         created_at: new Date(),
-//         vote_count: 10,
-//         comments: [{}, {}],
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
-
-
-// router.get('/post/:id', (req, res) => {
-//     // const post = {
-//     //   id: 1,
-//     //   post_url: 'https://handlebarsjs.com/guide/',
-//     //   title: 'Handlebars Docs',
-//     //   created_at: new Date(),
-//     //   vote_count: 10,
-//     //   comments: [{}, {}],
-//     //   user: {
-//     //     username: 'test_user'
-//     //   }
-//     // };
-  
-//     // res.render('single-post', { post });
-
-//     Post.findOne({
-//         where: {
-//           id: req.params.id
-//         },
-//         attributes: [
-//           'id',
-//           'post_url',
-//           'title',
-//           'created_at',
-//           [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-//         ],
-//         include: [
-//           {
-//             model: Comment,
-//             attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-//             include: {
-//               model: User,
-//               attributes: ['username']
-//             }
-//           },
-//           {
-//             model: User,
-//             attributes: ['username']
-//           }
-//         ]
-//       })
-//         .then(dbPostData => {
-//           if (!dbPostData) {
-//             res.status(404).json({ message: 'No post found with this id' });
-//             return;
-//           }
-    
-//           // serialize the data
-//           const post = dbPostData.get({ plain: true });
-    
-//           // pass data to template
-//           res.render('single-post', { post });
-//         })
-//         .catch(err => {
-//           console.log(err);
-//           res.status(500).json(err);
-//         });
-// });
-
-
-
-
-// module.exports = router;
-
 // will contain all of the user-facing routes, such as the homepage and login page
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
@@ -127,10 +34,7 @@ router.get('/', (req, res) => {
         .then(dbPostData => {
           // pass a single post object into the homepage template
           const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { 
-              posts, 
-              loggedIn: req.session.loggedIn 
-            });
+          res.render('homepage', { posts, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
           console.log(err);
@@ -185,10 +89,7 @@ router.get('/post/:id', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', {
-            post,
-            loggedIn: req.session.loggedIn
-          });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn});
       })
       .catch(err => {
         console.log(err);
