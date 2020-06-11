ffd126b6f8d123641847dbce90b727d7816df70f
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-06-10 22:59:50 -0700

    adding logic to helper functions 14.4.8

--- utils/helpers.js
+++ utils/helpers.js
@@ -17,11 +17,25 @@ module.exports = {
         return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()}`;
       },
 
+
+
     format_plural: (word, amount) => {
       if (amount !== 1) {
         return `${word}s`;
       }
   
       return word;
+    },
+
+
+
+
+    format_url: url => {
+        return url
+          .replace('http://', '')
+          .replace('https://', '')
+          .replace('www.', '')
+          .split('/')[0]
+          .split('?')[0]
     }
   }
